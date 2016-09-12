470a7bb49009ab2e1ec0a6cbc4fd66c8a949f76b
Peter Kirov <pkirov@renre.com>
2016-09-12 12:26:22 +0100

    Move header outside of the loop.Fix Typo.

--- palidroms/__main__.py
+++ palidroms/__main__.py
@@ -5,8 +5,8 @@ from functions import *
 Main routine
 '''
 def main():
+    print '"decimal", "smallest base in which the number is a palindrome"'
     for row in processPalidromCheck(0,1001):
-        print '"decimal", "smallest base in which the number is a palindrome"'
         print ",".join(map(str, row))
 
 if __name__ == "__main__":
